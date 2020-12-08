6cf68745eff1e10257761cb53d29742a437d857b
Cristopher Paolo Enciso Torres <cristopher.paolo.et@gmail.com>
2020-12-07 18:46:23 -0600

    Mobile form content (#4)
    * Add - Card and option for short link (styles and js function)
    
    * Refactory - Cards and circles with icons
    
    * Add - Changes and merge by footer branch
    
    * Fix - class out off media query
    
    Co-authored-by: DevCristopher <69356064+LWCristopher@users.noreply.github.com>

--- main.js
+++ main.js
@@ -5,3 +5,20 @@ var menuOptions = document.getElementById("menuOptions");
 btnBurger.addEventListener("click", function() {
     menuOptions.classList.toggle("hidden");
 });
+
+
+// shorten link
+var URL_BASE = 'https://api.shrtco.de/v2'
+var inptShorten = document.getElementById('inptShorten');
+var btnShorten = document.getElementById('btnShorten');
+
+btnShorten.addEventListener('click', () => {
+    console.log(inptShorten.value)
+    let link = inptShorten.value;
+
+    fetch(`${URL_BASE}/shorten?url=${link}`, {
+        method: 'POST'
+    })
+    .then(response => response.json())
+    .then(data => console.log(data));
+});

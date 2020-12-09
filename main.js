34fa8cafe70a69108b338e1a7df375b486dc39f0
DevCristopher <69356064+LWCristopher@users.noreply.github.com>
2020-12-08 19:18:12 -0600

    Merge - Styles and functions by Joge and Edson

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
-// Id's
+// Id's boton hamburguesa
 var btnBurger = document.getElementById("btnBurger");
 var menuOptions = document.getElementById("menuOptions");
 
-btnBurger.addEventListener("click", function() {
+btnBurger.addEventListener("click", function () {
     menuOptions.classList.toggle("hidden");
 });
-
 
 // shorten link
 var URL_BASE = 'https://api.shrtco.de/v2'
@@ -15,6 +14,15 @@ var btnShorten = document.getElementById('btnShorten');
 btnShorten.addEventListener('click', () => {
     console.log(inptShorten.value)
     let link = inptShorten.value;
+
+    if (!link) {
+        var error = document.getElementById('mensajeError');
+        error.style.display = 'block';
+        return false;
+    }
+
+    var error = document.getElementById('mensajeError');
+    error.style.display = 'none';
 
     fetch(`${URL_BASE}/shorten?url=${link}`, {
         method: 'POST'
